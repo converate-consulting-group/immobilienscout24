7e6d6cf981f1200627648be8561b92d19dc27c86
Spas Poptchev <spoptchev@x-rails.com>
2014-04-16 23:26:15 +0200

    publish and real estate api

--- lib/immobilienscout24/api/publish.rb
+++ lib/immobilienscout24/api/publish.rb
@@ -7,29 +7,69 @@ module Immobilienscout24
     # @see http://developerwiki.immobilienscout24.de/wiki/PublishChannel/GET
     module Publish
 
+      # Get a list of all publications
+      #
+      # @param estate [Hash] Estate id
+      # @param params [Hash] Additional request parameters
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/Publish/GET
+      # @example
+      #   client.publications(estate_id)
       def publications(estate, params = {})
         params = {realestate: estate}.merge(params)
         get publish_endpoint("/publish"), params
       end
 
+      # Get a single publication
+      #
+      # @param id [String] Publication id
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/Publish/GETbyID
+      # @example
+      #   client.publication(publication_id)
       def publication(id)
         get publish_endpoint("/publish/#{id}")
       end
 
+      # Create multiple publications
+      #
+      # @param publications [Hash] Publication data
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/Publish/POST
+      # @example
+      #   client.create_publications(publications)
       def create_publications(publications)
         post publish_endpoint("/publish/list"), publications
       end
 
+      # Create a single publication
+      #
+      # @param publication [Hash] Publication data
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/Publish/POSTbyID
+      # @example
+      #   client.create_publication(publication)
       def create_publication(publication)
         post publish_endpoint("/publish"), publication
       end
 
+      # Delete a publication
+      #
+      # @param id [String] Publication id
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/Publish/DELETEbyID
+      # @example
+      #   client.delete_publication(publication_id)
       def delete_publication(id)
         delete publish_endpoint("/publish/#{id}")
       end
 
       # -- endpoint
 
+      # Generates the publish endpoint
+      #
+      # @param resource [String, Integer] In most cases the id of the publication
+      # @return [String] The url to the publish resource
       def publish_endpoint(resource)
         ["api/offer/#{api_version}", resource].join
       end
--- lib/immobilienscout24/api/real_estate.rb
+++ lib/immobilienscout24/api/real_estate.rb
@@ -3,31 +3,83 @@ module Immobilienscout24
 
     # Methods for the RealEstate API
     #
+    # Per default the client will use the current user (`me`).
+    # If you want to use an other user then you have to provide
+    # the id in the options hash.
+    #
+    # @example
+    #   client.real_estate(663515214, user: immoscout_user_id)
+    #
     # @see http://developerwiki.immobilienscout24.de/wiki/User/Realestate
     module RealEstate
 
+      # Get a list of all real estates
+      #
+      # @param options [Hash] Additional options
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/User/Realestate/GETALL
+      # @example
+      #   client.real_estates
       def real_estates(options = {})
         get real_estate_endpoint("/realestate", options)
       end
 
+      # Get a single real estate
+      #
+      # @param id [String, Integer] Real estate id
+      # @param options [Hash] Additional options
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/User/Realestate/GETbyID
+      # @example
+      #   client.real_estate(real_estate_id)
       def real_estate(id, options = {})
         get real_estate_endpoint("/realestate/#{id}", options)
       end
 
+      # Create a real estate
+      #
+      # @param estate [Hash] Estate data
+      # @param options [Hash] Additional options
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/User/Realestate/POST
+      # @example
+      #   client.create_real_estate(estate)
       def create_real_estate(estate, options = {})
         post real_estate_endpoint("/realestate", options), estate
       end
 
+      # Update a real estate
+      #
+      # @param id [String, Integer] Real estate id
+      # @param estate [Hash] Estate data
+      # @param options [Hash] Additional options
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/User/Realestate/PUTbyID
+      # @example
+      #   client.update_real_estate(estate_id, data)
       def update_real_estate(id, estate, options = {})
         put real_estate_endpoint("/realestate/#{id}", options), estate
       end
 
+      # Delete a real estate
+      #
+      # @param id [String, Integer] Real estate id
+      # @param options [Hash] Additional options
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/User/Realestate/DELETEbyID
+      # @example
+      #   client.delete_real_estate(estate_id, data)
       def delete_real_estate(id, options = {})
         delete real_estate_endpoint("/realestate/#{id}", options)
       end
 
       # -- endpoint
 
+      # Generates the real estate endpoint
+      #
+      # @param resource [String, Integer] In most cases the id of the real estate
+      # @param options [Hash] Additional options
+      # @return [String] The url to the real estate resource
       def real_estate_endpoint(resource, options = {})
         options = {user: "me"}.merge(options)
         user_real_estate_endpoint(options.fetch(:user), resource, options)
