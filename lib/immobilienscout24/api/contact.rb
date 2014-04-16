23f808b79aaed757323e3efa5155696be4f95d9b
Spas Poptchev <spoptchev@x-rails.com>
2014-04-16 23:02:54 +0200

    contact api documentation and fix

--- lib/immobilienscout24/api/contact.rb
+++ lib/immobilienscout24/api/contact.rb
@@ -16,7 +16,7 @@ module Immobilienscout24
       # Get a list of all contacts
       #
       # @param options [Hash] Additional options
-      # @return [Hashie::Mash] Immobilienscout24 respons
+      # @return [Hashie::Mash] Immobilienscout24 response
       # @see http://developerwiki.immobilienscout24.de/wiki/User/Contact/GETALL
       # @example
       #   client.contacts
@@ -24,20 +24,50 @@ module Immobilienscout24
         get contact_endpoint("/contact", options)
       end
 
-      def contact(id)
-        get contact_endpoint("/contact/#{id}")
+      # Get a contact
+      #
+      # @param id [String, Integer] Contact id
+      # @param options [Hash] Additional options
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/User/Contact/GETbyID
+      # @example
+      #   client.contact(663515214)
+      def contact(id, options = {})
+        get contact_endpoint("/contact/#{id}", options)
       end
 
+      # Create a contact
+      #
+      # @param contact [Hash] The new contact
+      # @param options [Hash] Additional options
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/User/Contact/POST
+      # @example
+      #   client.create_contact(contact)
       def create_contact(contact, options = {})
         post contact_endpoint("/contact", options), contact
       end
 
+      # Update a contact
+      #
+      # @param id [String, Integer] Contact id
+      # @param contact [Hash] Data to update
+      # @param options [Hash] Additional options
+      # @return [Hashie::Mash] Immobilienscout24 response
+      # @see http://developerwiki.immobilienscout24.de/wiki/User/Contact/PUTbyID
+      # @example
+      #   client.update_contact(663515214, contact)
       def update_contact(id, contact, options = {})
         put contact_endpoint("/contact/#{id}", options), contact
       end
 
       # -- endpoint
 
+      # Generates the contact endpoint
+      #
+      # @param resource [String, Integer] In most cases the id of the contact
+      # @param options [Hash] Additional options
+      # @return [String] The url to the contact resource
       def contact_endpoint(resource, options = {})
         options = {user: "me"}.merge(options)
         user_contact_endpoint(options.fetch(:user), resource, options)
